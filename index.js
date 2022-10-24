ca1818f177bf1ee3a674c5a60285d8ed95d9de63
Robertas <r.ankudovicius@gmail.com>
2022-10-24 19:56:00 +0300

    put / delete added

--- index.js
+++ index.js
@@ -28,11 +28,32 @@ app.get("/getTasks", function (req, res) {
 app.post("/insertTask", function (req, res) {
   console.log(req.body.task);
   tasks.push({
+    id: req.body.id,
     task: req.body.task,
     isDone: req.body.isDone,
   });
 
   res.status(200).json({ statusMessage: "task was inserted successfully" });
+});
+
+app.put("/editTask/:id", function (req, res) {
+  const index = tasks.findIndex((task) => task.id === req.params.id);
+
+  tasks[index].task = req.body.editedTask;
+
+  console.log("task", tasks);
+
+  res.status(200).json({ statusMessage: "Eddited successfully" });
+});
+
+app.delete("/deleteTask/:id", function (req, res) {
+  const index = tasks.findIndex((task) => task.id === req.params.id);
+
+  const deletedValue = tasks.splice(index, 1);
+
+  console.log("task", tasks);
+
+  res.status(200).json({ deletedItem: deletedValue });
 });
 
 app.listen(3000);
